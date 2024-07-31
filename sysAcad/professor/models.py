a1f2eefbdd16205bbaacab5ef4afe5f4c7fb1717
RenannLimaa <renanroddev@gmail.com>
2024-07-31 15:56:30 -0300

    create professor model

--- sysAcad/professor/models.py
+++ sysAcad/professor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.CharField(max_length=32)
+    cellphone = models.CharField(max_length=14)
+    birth_date = models.DateField(auto_now=False, auto_now_add=False)
+    registration = models.CharField(max_length=8, primary_key=True)
+
+    def __str__(self):
+        return self.name
