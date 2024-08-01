94ac8becf9a808520c8c13e9f578550f381843bf
RenannLimaa <renanroddev@gmail.com>
2024-07-31 21:06:36 -0300

    hash password field

--- sysAcad/authentication/views.py
+++ sysAcad/authentication/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
 from .forms import StudentProfessorForm
 from student.models import Student
 from professor.models import Professor
@@ -14,22 +15,31 @@ def signup(request):
             email = form.cleaned_data['email']
             cellphone = form.cleaned_data['cellphone']
             birth_date = form.cleaned_data['birth_date']
+            password = form.cleaned_data['password']
+
+            hashed_password = make_password(password)
 
             if role == "professor":
-                Professor.objects.create(
+                professor = Professor.objects.create(
                     name=name,
                     email=email,
                     cellphone=cellphone,
-                    birth_date=birth_date
+                    birth_date=birth_date,
+                    password=hashed_password
                 )
 
+                professor.save()
+
             elif role == "student":
-                Student.objects.create(
+                student = Student.objects.create(
                     name=name,
                     email=email,
                     cellphone=cellphone,
-                    birth_date=birth_date
+                    birth_date=birth_date,
+                    password=hashed_password
                 )
+
+                student.save()
 
         return render(request, "authentication/signup.html", {'form': form})
     else:
