5f50f65c571d601a8dede6b825444d630c9ad2b4
RenannLimaa <renanroddev@gmail.com>
2024-07-31 21:22:26 -0300

    alter the admin page for Professor model

--- sysAcad/professor/admin.py
+++ sysAcad/professor/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Professor
 
+
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('registration', 'name', 'email', 'cellphone', 'birth_date')
+    search_fields = ('name', 'email', 'registration')
+
+
 admin.site.register(Professor)
