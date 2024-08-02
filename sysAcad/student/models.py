09c882932c56b16dd8e9c2d798c610aefb548700
RenannLimaa <renanroddev@gmail.com>
2024-08-02 15:15:36 -0300

    user User model for authenticating professor and student

--- sysAcad/student/models.py
+++ sysAcad/student/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
 
 
 class Student(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+    email = models.EmailField()
     name = models.CharField(max_length=64)
-    email = models.CharField(max_length=32)
     cellphone = models.CharField(max_length=14)
     birth_date = models.DateField(auto_now=False, auto_now_add=False)
-    registration = models.BigAutoField(primary_key=True)
-    password = models.CharField(max_length=128)
 
     def __str__(self):
         return self.name
