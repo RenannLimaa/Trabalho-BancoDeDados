2bebc0e4e262390e06e56d7dac539a1f3197f927
RenannLimaa <renanroddev@gmail.com>
2024-08-11 15:23:02 -0300

    rename view edit_info to edit_student

--- sysAcad/student/urls.py
+++ sysAcad/student/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='student_home'),
     path('remove_student/', views.remove_student, name='remove_student'),
-    path('home/edit_info/', views.edit_student, name='edit_info'),
+    path('home/edit_info/', views.edit_student, name='edit_student'),
 ]
