c85ecb604b5f15da8821130c6437e89040d2bcc8
RenannLimaa <renanroddev@gmail.com>
2024-08-11 14:53:27 -0300

    add functionality to edit student information

--- sysAcad/student/urls.py
+++ sysAcad/student/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='student_home'),
-    path('remove_student/', views.remove_student, name='remove_student')
+    path('remove_student/', views.remove_student, name='remove_student'),
+    path('home/edit_info/', views.edit_student, name='edit_info'),
 ]
--- sysAcad/student/views.py
+++ sysAcad/student/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from users.models import User
+from users.forms import EditInfoForm
 
 from .models import Student
 
@@ -23,7 +24,7 @@ def remove_student(request):
             user.delete()
             messages.success(request, "Conta excluída com sucesso")
             return redirect("login")
-        except User.DoesNotExist:
+        except Student.DoesNotExist:
             messages.error(request, "Erro ao excluir a conta")
             return redirect(reverse("student_home"))
 
@@ -33,4 +34,49 @@ def remove_student(request):
 
 
 def edit_student(request):
-    pass
+    try:
+        student = Student.objects.get(user=request.user)
+
+    except Student.DoesNotExist:
+        messages.error(request, "Estudante não encontrado")
+        return redirect(reverse("student_home"))
+
+    if request.method == "POST":
+        form = EditInfoForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data.get("name")
+            email = form.cleaned_data.get("email")
+            cellphone = form.cleaned_data.get("cellphone")
+            password = form.cleaned_data.get("password")
+            birth_date = form.cleaned_data.get("birth_date")
+
+            student.name = name
+            student.email = email
+            student.cellphone = cellphone
+            student.password = password
+            student.birth_date = birth_date
+
+            user = student.user
+            user.username = email
+            if password:
+                user.set_password(password)
+            user.save()
+            student.save()
+            login(request, user)
+
+            messages.success(request, "Informações atualizadas com sucesso.")
+            return redirect(reverse("student_home"))
+
+        else:
+            messages.error(request, "Erro ao atualizar informações")
+
+    else:
+        form = EditInfoForm(
+            initial={
+                "name": student.name,
+                "email": student.email,
+                "cellphone": student.cellphone,
+                "birth_date": student.birth_date,
+            }
+        )
+    return render(request, "student/edit_info.html", {"form": form})
