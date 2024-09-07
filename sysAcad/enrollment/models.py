43f354e0af89058c6f1231d675b6d02e3811884c
RenannLimaa <renanroddev@gmail.com>
2024-09-07 15:33:30 -0300

    create Enrollment model

---
+++ sysAcad/enrollment/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from student.models import Student
+from course.models import Course
+from subject.models import Subject
+
+
+class Enrollment(models.Model):
+    student = models.ForeignKey(Student, related_name='matriculas', on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, related_name='matriculas', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, related_name='matriculas', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("student", "course", "subject")
+
+    def __str__(self):
+        return f"{self.aluno.nome} - {self.curso.nome} - {self.disciplina.nome}"
