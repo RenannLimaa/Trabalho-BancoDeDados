5a4222e1ba1b8e6b068bae43da94f4b2d7d171c6
RenannLimaa <renanroddev@gmail.com>
2024-09-07 15:32:44 -0300

    create Subject model

--- sysAcad/subject/models.py
+++ sysAcad/subject/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    vacancies = models.PositiveIntegerField()
+    course = models.ForeignKey("course.Course", on_delete=models.CASCADE, related_name="subjects")
+
+    def __str__(self):
+        return self.nome
