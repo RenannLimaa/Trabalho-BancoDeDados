96037130bba633abf704ab3eb4852fe1f7633223
RenannLimaa <renanroddev@gmail.com>
2024-09-07 15:21:17 -0300

    create classes model

--- sysAcad/classes/models.py
+++ sysAcad/classes/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Classes(models.Model):
+    DAYS_OF_WEEK = [
+        ('Monday', 'Monday'),
+        ('Tuesday', 'Tuesday'),
+        ('Wednesday', 'Wednesday'),
+        ('Thursday', 'Thursday'),
+        ('Friday', 'Friday'),
+        ('Saturday', 'Saturday'),
+        ('Sunday', 'Sunday'),
+    ]
+
+    id = models.AutoField(primary_key=True)
+    classroom = models.CharField(max_length=32)
+    start_time = models.TimeField()
+    day_of_week = models.CharField(max_length=10, choices=DAYS_OF_WEEK)
+    end_time = models.TimeField()
+    subject = models.ForeignKey("subject.Subject", on_delete=models.CASCADE, related_name="classes")
+    professor = models.ForeignKey("professor.Professor", on_delete=models.CASCADE, related_name="classes")
+
+    def __str__(self):
+        return self.classroom
