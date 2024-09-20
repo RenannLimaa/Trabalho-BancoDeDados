0013093c31e62ba48ec91de320ca1f7cd63dc295
RenannLimaa <renanroddev@gmail.com>
2024-09-20 17:06:46 -0300

    add day model and relationship with classes

--- sysAcad/classes/models.py
+++ sysAcad/classes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Classes(models.Model):
+class Day(models.Model):
     DAYS_OF_WEEK = [
         ('Monday', 'Monday'),
         ('Tuesday', 'Tuesday'),
@@ -12,11 +12,21 @@ class Classes(models.Model):
         ('Sunday', 'Sunday'),
     ]
 
+    name = models.CharField(max_length=9, choices=DAYS_OF_WEEK, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Classes(models.Model):
     id = models.AutoField(primary_key=True)
     classroom = models.CharField(max_length=32)
+    vacancies = models.PositiveIntegerField(default=10)
+
     start_time = models.TimeField()
-    day_of_week = models.CharField(max_length=10, choices=DAYS_OF_WEEK)
     end_time = models.TimeField()
+    days_of_week = models.ManyToManyField(Day, related_name="classes")
+
     subject = models.ForeignKey("subject.Subject", on_delete=models.CASCADE, related_name="classes")
     professor = models.ForeignKey("professor.Professor", on_delete=models.CASCADE, related_name="classes")
 
