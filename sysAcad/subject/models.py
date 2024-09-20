44f194cc7c13b41ec173bfd8e6d822b734935e20
RenannLimaa <renanroddev@gmail.com>
2024-09-20 17:10:34 -0300

    fix name attribute in __str__ method

--- sysAcad/subject/models.py
+++ sysAcad/subject/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Subject(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    vacancies = models.PositiveIntegerField()
     course = models.ForeignKey("course.Course", on_delete=models.CASCADE, related_name="subjects")
 
     def __str__(self):
-        return self.nome
+        return self.name
