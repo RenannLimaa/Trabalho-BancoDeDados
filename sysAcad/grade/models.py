b0ef398341a26759594484c8ccd72678173f9492
brun0m <brun0m.sousa@aluno.uece.br>
2024-09-29 21:31:01 -0300

    conserto

--- sysAcad/grade/models.py
+++ sysAcad/grade/models.py
@@ -3,9 +3,15 @@ from django.db import models
 
 class Grade(models.Model):
     id = models.AutoField(primary_key=True)
-    grade = models.DecimalField(max_digits=5, decimal_places=2)
     student = models.ForeignKey("student.Student", related_name='notas', on_delete=models.CASCADE)
     subject = models.ForeignKey("subject.Subject", related_name='notas', on_delete=models.CASCADE)
+    grade1 = models.FloatField(null=True, blank=True)
+    grade2 = models.FloatField(null=True, blank=True)
+    grade3 = models.FloatField(null=True, blank=True)
+
+    def calculate_average(self):
+        grades = [g for g in [self.grade1, self.grade2, self.grade3] if g is not None]
+        return sum(grades) / len(grades) if grades else None
 
     def __str__(self):
         return f"Nota para {self.aluno.nome} em {self.disciplina.nome}: {self.nota}"
